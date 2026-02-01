8e878914187661f1cfb2536bbf109d5ae58a4344
faizul.khan <faizul.khan@dngroup.com>
2026-02-01 10:05:14 +0600

    change code

--- src/HelloWorld/Program.cs
+++ src/HelloWorld/Program.cs
@@ -1,7 +1,7 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello, World!");
+app.MapGet("/", () => "Hello, World2!");
 
 app.Run("http://0.0.0.0:80");
 
