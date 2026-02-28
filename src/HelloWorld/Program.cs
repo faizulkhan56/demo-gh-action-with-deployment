0c6b121316099cbd2562527a8e567828bd951d65
faizul.khan <faizul.khan@dngroup.com>
2026-03-01 00:11:35 +0600

    class change in demo

--- src/HelloWorld/Program.cs
+++ src/HelloWorld/Program.cs
@@ -1,7 +1,7 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello, World5!");
+iapp.MapGet("/", () => "Hello, World5-ntech-devops class!");
 
 app.Run("http://0.0.0.0:80");
 
