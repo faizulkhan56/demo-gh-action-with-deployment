c8368d3810b6085a4e5061d5257a63ec7180848a
faizul.khan <faizul.khan@dngroup.com>
2026-05-17 23:00:09 +0600

    test for webinner

--- src/HelloWorld/Program.cs
+++ src/HelloWorld/Program.cs
@@ -1,7 +1,7 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello, World6-ntech-devops class-test!");
+app.MapGet("/", () => "Hello, World7-ntech-devops class-test!");
 
 app.Run("http://0.0.0.0:80");
 
