64b78a408f83e16655876e2bf2900e6f3781b5fb
faizul.khan <faizul.khan@dngroup.com>
2026-05-17 19:33:15 +0600

    test

--- src/HelloWorld/Program.cs
+++ src/HelloWorld/Program.cs
@@ -1,7 +1,7 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello, World5-ntech-devops class!");
+app.MapGet("/", () => "Hello, World5-ntech-devops class-test!");
 
 app.Run("http://0.0.0.0:80");
 
