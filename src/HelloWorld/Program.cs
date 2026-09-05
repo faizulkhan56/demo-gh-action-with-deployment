e934ee3009bec2ff667950f6cf52c1b7a337576c
faizul.khan <faizul.khan@dngroup.com>
2026-09-05 12:33:38 +0600

    change ntech

--- src/HelloWorld/Program.cs
+++ src/HelloWorld/Program.cs
@@ -1,7 +1,7 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello, World8-ntech-devops class-test!");
+app.MapGet("/", () => "Hello, World-ntech-devops class-test!");
 
 app.Run("http://0.0.0.0:80");
 
